9bf58c88790e75c1aa8e8e4ce0d96a68469bda64
Mysti Quant <mystiquant@icloud.com>
2024-09-13 09:54:28 -0700

    reformatting repo

---
+++ src/_App.jsx
@@ -0,0 +1,11 @@
+// import './App.css'
+// import Board from './components/Board'
+
+// export default function App() {
+//   return (
+//     <div>
+//       <Board/>
+//     </div>
+//   )
+// }
+
